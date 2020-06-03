2eb226aa714ac8307bd5bc3cef5e5677d665e555
Antoine Cadoret <antoinecadoret97@gmail.com>
2020-06-03 18:06:36 -0400

    component optimization et style

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Vinyle from './components/Vinyle';
 import './App.css';
 
 class App extends Component {
-  state ={
-    // variable qui va contenir les donnees de la BD
-    vinyles:[],
-    // variable qui va contenir les informations du vinyle choisi
-    vinyle:[]
-  }
-  componentDidMount(){
-    // connection a l'api
-    axios.get('https://api.discogs.com/users/ausamerika/collection/folders/0/releases')
-      .then(response => {
-            // mettre les donnees de la BD dans la variable vinyles
-              const vinylesData = response.data.releases;
-              this.setState({vinyles : vinylesData});
-              this.choisirAleatoirementUnVinyle(vinylesData);
-    });
-  }
-  // fonction pour choisir aléatoirement un vinyle 
-  choisirAleatoirementUnVinyle = (tableVinyles) => {
-    console.log(tableVinyles.length);
-    const nbAleatoire = Math.floor(Math.random()*tableVinyles.length);
-    console.log(nbAleatoire);
-    this.setState({vinyle : tableVinyles[nbAleatoire]});
-    console.log(this.state.vinyle);
-
-  }
   
-
-  
- 
 
   render() {
-    // const vinyles = this.state.vinyles.map(vinyle => {
-    //     return 
-    // });
-    const tbVinyles = this.state.vinyles;
     return (
       <div className="App">
-        <button onClick={() => this.choisirAleatoirementUnVinyle(tbVinyles)}>choisir un autre vinyle</button>
+        <h1>discogs</h1>
+        <Vinyle />
       </div>
     );
   }
--- src/components/Vinyle.js
+++ src/components/Vinyle.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-const vinyle = (props) => {
-    return (
-        <div className="vinyle">
+class Vinyle extends Component{
+    state = {
+      // variable qui va contenir les donnees de la BD
+      vinyles:[],
+      // variable qui va contenir les informations du vinyle choisi
+      vinyle:[]
+    }
+    
+    componentDidMount(){
+    // connection a l'api
+        axios.get('https://api.discogs.com/users/ausamerika/collection/folders/0/releases')
+          .then(response => {
+                // mettre les donnees de la BD dans la variable vinyles
+                  const vinylesData = response.data.releases;
+                  this.setState({vinyles : vinylesData});
+                  this.choisirAleatoirementUnVinyle(vinylesData);
+        });
+      }
+      
+      // fonction pour choisir aléatoirement un vinyle 
+      choisirAleatoirementUnVinyle = (tableVinyles) => {
+        const nbAleatoire = Math.floor(Math.random()*tableVinyles.length);
+        this.setState({vinyle : tableVinyles[nbAleatoire].basic_information});
+        console.log(this.state.vinyle);
+      }
+      
+    
+      
+     
+    
+      render() {
+        const artists = [];
 
-        </div>
-    );
-}; 
-export default vinyle;
+        for(let artistName in this.state.vinyle.artists){
+          artists.push({
+            id:artistName, 
+            name : this.state.vinyle.artists[artistName].name
+          });
+        }
+        const artistOutput = artists.map((artist) => {
+            return <ListGroup.Item>{artist.name}</ListGroup.Item>
+        });
+
+        const tbVinyles = this.state.vinyles;
+          return(
+            <div className="vinyle" style={{maxWidth: '500px', margin: '0 auto'}}>
+                <Button variant='outline-info' onClick={() => this.choisirAleatoirementUnVinyle(tbVinyles)}>Choisir un autre vinyle</Button>
+                <Card style={{marginTop: '30px'}}>
+                    <Card.Body>
+                      <Card.Title>{this.state.vinyle.title}</Card.Title>
+                      <Card.Subtitle>{this.state.vinyle.year}</Card.Subtitle>
+
+                      <ListGroup>{artistOutput}</ListGroup>
+                      <Card.Text>AuthorAuthorAuthorAuthorAuthorAuthor</Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+          )
+              
+        }
+   
+
+}
+export default  Vinyle;
