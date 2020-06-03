2fdcc1806c2ab3a3ebb067e4ef26c81880c22fc2
Antoine Cadoret <antoinecadoret97@gmail.com>
2020-06-03 14:44:55 -0400

    mettre les donnees dans une variable

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  
+  state ={
+    // variable qui va contenir les donnees de la BD
+    venyles:[]
+  }
   componentDidMount(){
     // connection a l'api
     axios.get('https://api.discogs.com/users/ausamerika/collection/folders/0/releases')
             .then(response => {
-                console.log(response);
+              // mettre les donnees de la BD dans la variable venyles
+                this.setState({venyles : response.data.releases})
+                console.log(response.data);
             });
   }
   render() {
+    // const venyles = this.state.venyles.map(venyle => {
+    //     return 
+    // });
     return (
       <div className="App">
       
